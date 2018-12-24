c829b4f5bac61a93de18f3404f5895810b087b0b
David Tolnay <dtolnay@gmail.com>
2018-12-24 11:35:03 -0500

    Typed distributed plugin registration

---
+++ impl/src/lib.rs
@@ -0,0 +1,50 @@
+extern crate proc_macro;
+
+use std::collections::hash_map;
+use std::hash::Hasher;
+
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::quote;
+use syn::parse_macro_input;
+
+#[proc_macro]
+#[doc(hidden)]
+pub fn submit(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let expr = parse_macro_input!(input as TokenStream);
+    let init = Ident::new(&format!("__init{}", hash(&expr)), Span::call_site());
+
+    let expanded = quote! {
+        #[inventory::ctor]
+        fn #init() {
+            // TODO: once existential type is stable, store the caller's
+            // expression into a static and string those statics together into
+            // an intrusive linked list without needing allocation.
+            //
+            //     existential type This;
+            //
+            //     static mut VALUE: Option<inventory::Node<This>> = None;
+            //
+            //     fn value() -> This {
+            //         #expr
+            //     }
+            //
+            //     unsafe {
+            //         VALUE = Some(inventory::Node {
+            //             value: value(),
+            //             next: None,
+            //         };
+            //         inventory::submit(VALUE.as_mut().unwrap());
+            //     }
+
+            inventory::submit({ #expr });
+        }
+    };
+
+    proc_macro::TokenStream::from(expanded)
+}
+
+fn hash(input: &TokenStream) -> u64 {
+    let mut hasher = hash_map::DefaultHasher::new();
+    hasher.write(input.to_string().as_bytes());
+    hasher.finish()
+}
---
+++ src/lib.rs
@@ -0,0 +1,334 @@
+//! **Typed distributed plugin registration.**
+//!
+//! This crate provides a way to set up a plugin registry into which plugins
+//! can be registered from any source file linked into your application. There
+//! does not need to be a central list of all the plugins.
+//!
+//! # Examples
+//!
+//! Suppose we are writing a command line flags library and want to allow any
+//! source file in the application to register command line flags that are
+//! relevant to it.
+//!
+//! This is the flag registration style used by [gflags] and is better suited
+//! for large scale development than maintaining a single central list of flags,
+//! as the central list would become an endless source of merge conflicts in an
+//! application developed simultaneously by thousands of developers.
+//!
+//! [gflags]: https://gflags.github.io/gflags/
+//!
+//! ## Instantiating the plugin registry
+//!
+//! Let's use a `struct Flag` as the plugin type, which will contain the short
+//! name of the flag like `-v`, the full name like `--verbose`, and maybe other
+//! information like argument type and help text. We instantiate a plugin
+//! registry with an invocation of `inventory::collect!`.
+//!
+//! ```
+//! pub struct Flag {
+//!     short: char,
+//!     name: &'static str,
+//!     /* ... */
+//! }
+//!
+//! impl Flag {
+//!     pub fn new(short: char, name: &'static str) -> Self {
+//!         Flag { short, name }
+//!     }
+//! }
+//!
+//! inventory::collect!(Flag);
+//! ```
+//!
+//! This `collect!` call must be in the same crate that defines the plugin type.
+//! This macro does not "run" anything so place it outside of any function body.
+//!
+//! ## Registering plugins
+//!
+//! Now any crate with access to the `Flag` type can register flags as a plugin.
+//! Plugins can be registered by the same crate that declares the plugin type,
+//! or by any downstream crate.
+//!
+//! ```
+//! # struct Flag;
+//! #
+//! # impl Flag {
+//! #     fn new(short: char, name: &'static str) -> Self {
+//! #         Flag
+//! #     }
+//! # }
+//! #
+//! # inventory::collect!(Flag);
+//! #
+//! inventory::submit! {
+//!     Flag::new('v', "verbose")
+//! }
+//! #
+//! # fn main() {}
+//! ```
+//!
+//! The `submit!` macro does not "run" anything so place it outside of any
+//! function body. In particular, note that all `submit!` invocations across all
+//! source files linked into your application all take effect simultaneously. A
+//! `submit!` invocation is not a statement that needs to be called from `main`
+//! in order to execute.
+//!
+//! ## Iterating over plugins
+//!
+//! The value `inventory::iter::<T>` is an iterator with element type `&'static
+//! T` that iterates over all plugins registered of type `T`.
+//!
+//! ```
+//! # struct Flag {
+//! #     short: char,
+//! #     name: &'static str,
+//! # }
+//! #
+//! # inventory::collect!(Flag);
+//! #
+//! for flag in inventory::iter::<Flag> {
+//!     println!("-{}, --{}", flag.short, flag.name);
+//! }
+//! ```
+//!
+//! There is no guarantee about the order that plugins of the same type are
+//! visited by the iterator. They may be visited in any order.
+
+// Not public API.
+#[doc(hidden)]
+pub use ctor::ctor;
+
+// Not public API.
+#[doc(hidden)]
+pub use inventory_impl as r#impl;
+
+use core::ptr;
+use core::sync::atomic::{AtomicPtr, Ordering};
+
+// Not public API. Used by generated code.
+#[doc(hidden)]
+pub struct Registry<T: 'static> {
+    head: AtomicPtr<Node<T>>,
+}
+
+struct Node<T: 'static> {
+    value: T,
+    next: Option<&'static Node<T>>,
+}
+
+// Not public API. Used by generated code.
+#[doc(hidden)]
+pub trait Collect: Sized + 'static {
+    fn registry() -> &'static Registry<Self>;
+}
+
+// Not public API. Used by generated code.
+#[doc(hidden)]
+pub fn submit<T: Collect>(value: T) {
+    // TODO: Avoid allocation by storing node in a static mut Option<Node<T>>
+    // after existential type is stable. See comment in inventory-impl.
+    T::registry().submit(Box::leak(Box::new(Node { value, next: None })));
+}
+
+impl<T: 'static> Registry<T> {
+    // Not public API. Used by generated code.
+    pub const fn new() -> Self {
+        Registry {
+            head: AtomicPtr::new(ptr::null_mut()),
+        }
+    }
+
+    fn submit(&'static self, new: &'static mut Node<T>) {
+        let mut head = self.head.load(Ordering::SeqCst);
+        loop {
+            let prev = self.head.compare_and_swap(head, new, Ordering::SeqCst);
+            if prev == head {
+                // Pointer is always null or valid &'static Node<T>.
+                new.next = unsafe { prev.as_ref() };
+                return;
+            } else {
+                head = prev;
+            }
+        }
+    }
+}
+
+mod private {
+    use ghost::phantom;
+
+    #[phantom]
+    #[allow(non_camel_case_types)]
+    pub struct iter<T>;
+}
+
+/// An iterator over plugins registered of a given type.
+///
+/// The value `inventory::iter::<T>` is an iterator with element type `&'static
+/// T`.
+///
+/// There is no guarantee about the order that plugins of the same type are
+/// visited by the iterator. They may be visited in any order.
+///
+/// # Examples
+///
+/// ```
+/// # struct Flag {
+/// #     short: char,
+/// #     name: &'static str,
+/// # }
+/// #
+/// # inventory::collect!(Flag);
+/// #
+/// # const IGNORE: &str = stringify! {
+/// use my_flags::Flag;
+/// # };
+///
+/// fn main() {
+///     for flag in inventory::iter::<Flag> {
+///         println!("-{}, --{}", flag.short, flag.name);
+///     }
+/// }
+/// ```
+///
+/// Refer to the [crate level documentation](index.html) for a complete example
+/// of instantiating a plugin registry and submitting plugins.
+#[allow(non_camel_case_types)]
+pub type iter<T> = private::iter<T>;
+
+#[doc(hidden)]
+pub use crate::private::*;
+
+const ITER: () = {
+    impl<T: Collect> IntoIterator for iter<T> {
+        type Item = &'static T;
+        type IntoIter = Iter<T>;
+
+        fn into_iter(self) -> Self::IntoIter {
+            let head = T::registry().head.load(Ordering::SeqCst);
+            Iter {
+                // Head pointer is always null or valid &'static Node<T>.
+                node: unsafe { head.as_ref() },
+            }
+        }
+    }
+
+    pub struct Iter<T: 'static> {
+        node: Option<&'static Node<T>>,
+    }
+
+    impl<T: 'static> Iterator for Iter<T> {
+        type Item = &'static T;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            let node = self.node?;
+            let value = &node.value;
+            self.node = node.next;
+            Some(value)
+        }
+    }
+};
+
+/// Associate a plugin registry with the specified type.
+///
+/// This call must be in the same crate that defines the plugin type. This macro
+/// does not "run" anything so place it outside of any function body.
+///
+/// # Examples
+///
+/// Suppose we are writing a command line flags library and want to allow any
+/// source file in the application to register command line flags that are
+/// relevant to it.
+///
+/// This is the flag registration style used by [gflags] and is better suited
+/// for large scale development than maintaining a single central list of flags,
+/// as the central list would become an endless source of merge conflicts.
+///
+/// [gflags]: https://gflags.github.io/gflags/
+///
+/// ```
+/// pub struct Flag {
+///     short: char,
+///     name: &'static str,
+///     /* ... */
+/// }
+///
+/// inventory::collect!(Flag);
+/// ```
+///
+/// Refer to the [crate level documentation](index.html) for a complete example
+/// of submitting plugins and iterating a plugin registry.
+#[macro_export]
+macro_rules! collect {
+    ($ty:ty) => {
+        impl $crate::Collect for $ty {
+            #[inline]
+            fn registry() -> &'static $crate::Registry<Self> {
+                static REGISTRY: $crate::Registry<$ty> = $crate::Registry::new();
+                &REGISTRY
+            }
+        }
+    };
+}
+
+/// Enter an element into the plugin registry corresponding to its type.
+///
+/// This call may be in the same crate that defines the type, or downstream in
+/// any crate that depends on that crate.
+///
+/// This macro does not "run" anything so place it outside of any function body.
+/// In particular, note that all `submit!` invocations across all source files
+/// linked into your application all take effect simultaneously. A `submit!`
+/// invocation is not a statement that needs to be called from `main` in order
+/// to execute.
+///
+/// # Examples
+///
+/// Put `submit!` invocations outside of any function body.
+///
+/// ```
+/// # struct Flag;
+/// #
+/// # impl Flag {
+/// #     fn new(short: char, name: &'static str) -> Self {
+/// #         Flag
+/// #     }
+/// # }
+/// #
+/// # inventory::collect!(Flag);
+/// #
+/// inventory::submit! {
+///     Flag::new('v', "verbose")
+/// }
+/// #
+/// # fn main() {}
+/// ```
+///
+/// Do not try to invoke `submit!` from inside of a function body as it does not
+/// do what you want.
+///
+/// ```compile_fail
+/// // Do not do this.
+/// fn submit_flags(has_verbose_flag: bool) {
+///     if has_verbose_flag {
+///         inventory::submit! {
+///             Flag::new('v', "verbose")
+///         }
+///     }
+/// }
+/// ```
+///
+/// Refer to the [crate level documentation](index.html) for a complete example
+/// of instantiating and iterating a plugin registry.
+#[macro_export]
+macro_rules! submit {
+    ($($value:tt)*) => {
+        $crate::r#impl::submit! {
+            $($value)*
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn unused() {
+    let _ = ITER;
+}
