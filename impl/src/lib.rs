5a561d37b3dc0eebb9585f048422b5557f274755
David Tolnay <dtolnay@gmail.com>
2019-01-06 19:59:20 -0800

    Accept a re-export prefix

--- impl/src/lib.rs
+++ impl/src/lib.rs
@@ -5,16 +5,51 @@ use std::hash::Hasher;
 
 use proc_macro2::{Ident, Span, TokenStream};
 use quote::quote;
-use syn::parse_macro_input;
+use syn::parse::{Parse, ParseStream, Result};
+use syn::{bracketed, parse_macro_input, Token};
+
+struct Input {
+    krate: Option<Ident>,
+    expr: TokenStream,
+}
+
+impl Parse for Input {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Ok(Input {
+            krate: {
+                // #![crate = gflags]
+                if input.peek(Token![#]) && input.peek2(Token![!]) {
+                    input.parse::<Token![#]>()?;
+                    input.parse::<Token![!]>()?;
+                    let content;
+                    bracketed!(content in input);
+                    content.parse::<Token![crate]>()?;
+                    content.parse::<Token![=]>()?;
+                    let krate: Ident = content.parse()?;
+                    Some(krate)
+                } else {
+                    None
+                }
+            },
+            expr: input.parse()?,
+        })
+    }
+}
 
 #[proc_macro]
 #[doc(hidden)]
 pub fn submit(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let expr = parse_macro_input!(input as TokenStream);
+    let input = parse_macro_input!(input as Input);
+
+    let expr = input.expr;
     let init = Ident::new(&format!("__init{}", hash(&expr)), Span::call_site());
+    let prefix = match input.krate {
+        Some(krate) => quote!(#krate::),
+        None => quote!(),
+    };
 
     let expanded = quote! {
-        #[inventory::ctor]
+        #[#prefix inventory::ctor]
         fn #init() {
             // TODO: once existential type is stable, store the caller's
             // expression into a static and string those statics together into
@@ -36,7 +71,7 @@ pub fn submit(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
             //         inventory::submit(VALUE.as_mut().unwrap());
             //     }
 
-            inventory::submit({ #expr });
+            #prefix inventory::submit({ #expr });
         }
     };
 
